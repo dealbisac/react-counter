801f3141c579353c4168df0031a9a54ec5c72a55
dealbisac <chand.dipendra19@gmail.com>
2020-10-14 17:10:30 +0545

    lifecycle methods

--- src/Application.js
+++ src/Application.js
@@ -2,10 +2,39 @@ import React, { Component } from 'react';
 import './Application.css';
 
 class Application extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount(props, state) {
+
+  }
+
+  componentDidMount(props, state) {
+    console.log("Mounted with", props, state);
+  }
+
+  componentWillReceiveProps(props) {
+
+  }
+
+  componentWillUpdate(props, state) {
+    if (this.props.name !== props.name) {
+      //do something
+    }
+
+  }
+
+  componentDidUpdate(props, state) {
+
+  }
+
   render() {
+    let name = "Dipendra";
+
     return (
       <div className="App">
-        <h1> Hello from Class Component</h1>
+        <h1> Hello <i>{name}</i> from Class Component</h1>
 
       </div>
     )
