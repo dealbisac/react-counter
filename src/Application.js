a57be1757dc1b900a8b66ea610fe61074bed9226
dealbisac <chand.dipendra19@gmail.com>
2020-10-14 15:10:23 +0545

    class Application created

---
+++ src/Application.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Application.css';
+
+class Application extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Hello from Class Component</h1>
+
+      </div>
+    )
+  }
+}
+
+export default Application;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Application from './Application';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Application />
   </React.StrictMode>,
   document.getElementById('root')
 );
